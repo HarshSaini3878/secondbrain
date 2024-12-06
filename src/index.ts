68c694b5a77454a9ef7170b370879a3c3e3e2b43
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-12-06 13:00:56 +0530

    start

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import  express  from "express";
+import jwt from "jsonwebtoken";
+const app= express();
+app.use(express.json());
+
+
+app.post("/api/v1/signin",(req,res)=>{
+    const {username ,password}=req.
+
+})
+app.post("/api/v1/signup",(req,res)=>{
+
+
+})
